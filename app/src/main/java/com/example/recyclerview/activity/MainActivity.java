11422c113afcfdcf23031b157a0b27069cae4960
Walter Silva <walter.sjr@hotmail.com>
2023-01-21 17:14:27 -0300

    Projeto RecyclerView

--- app/src/main/java/com/example/recyclerview/activity/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/activity/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.recyclerview.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.recyclerview.R;
+import com.example.recyclerview.RecyclerItemClickListener;
 import com.example.recyclerview.adapter.Adapter;
 import com.example.recyclerview.model.Filme;
 
@@ -25,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
+        //Listagem de filmes
+        this.criarFilmes();
+
         //Configurar adapter
         Adapter adapter = new Adapter(listaFilme);
 
@@ -32,12 +41,47 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
         recyclerView.setAdapter(adapter);
+
+        //Evento de click
+        recyclerView.addOnItemTouchListener(
+             new RecyclerItemClickListener(
+                     getApplicationContext(),
+                     recyclerView,
+                     new RecyclerItemClickListener.OnItemClickListener() {
+                         @Override
+                         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                             Toast.makeText(
+                                     getApplicationContext(),
+                                     "Item pressionado",
+                                     Toast.LENGTH_SHORT
+                             ).show();
+                         }
+
+                         @Override
+                         public void onItemClick(View view, int position) {
+                             Toast.makeText(
+                                     getApplicationContext(),
+                                     "Click longo",
+                                     Toast.LENGTH_SHORT
+                             ).show();
+                         }
+
+                         @Override
+                         public void onLongItemClick(View view, int position) {
+
+                         }
+                     }
+             )
+
+        );
+
     }
     public void criarFilmes(){
         Filme filme = new Filme("Homem Aranha - De volta ao lar", "Aventura", "2017");
         this.listaFilme.add( filme );
-        filme = new Filme("Mulher Maravilha", "Fantasia", "2017");
+        filme = new Filme("Mulher Maravilha", "Fantasia",                     "2017");
         this.listaFilme.add( filme );
         filme = new Filme("Liga da Justiça", "Ficção", "2017");
         this.listaFilme.add( filme );
@@ -55,5 +99,6 @@ public class MainActivity extends AppCompatActivity {
         this.listaFilme.add( filme );
         filme = new Filme("Carros 3", "Comédia", "2017");
         this.listaFilme.add(filme);
+
     }
 }
